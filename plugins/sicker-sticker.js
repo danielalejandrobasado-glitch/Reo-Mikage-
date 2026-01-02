e09de867d14738d36d33e71ae51965ef1a6c1d51
ANDERSONTELLO <jhorditelloarrue@gmail.com>
2026-01-02 17:54:18 -0500

    Update sicker-sticker.js

--- plugins/sicker-sticker.js
+++ plugins/sicker-sticker.js
@@ -13,8 +13,8 @@ let now = new Date()
 let fecha = now.toLocaleDateString('es-ES', { day: '2-digit', month: '2-digit', year: 'numeric' })
 let hora = now.toLocaleTimeString('es-ES', { hour: '2-digit', minute: '2-digit' })
 
-let texto1 = packstickers.text1 || `╔═════ஜ۩۞۩ஜ═════╗\n𝐍 𝐀 𝐌 𝐄\n${userName || dev}\n╚═════ஜ۩۞۩ஜ═════╝`
-let texto2 = packstickers.text2 || `╔═════ஜ۩۞۩ஜ═════╗\n𝐁 𝐎 𝐓\n${botname}\n${fecha}\n${hora}\n╚═════ஜ۩۞۩ஜ═════╝`
+let texto1 = packstickers.text1 || `╔═════ஜ۩۞۩ஜ═════╗\n𝐍 𝐀 𝐌 𝐄\n${userName || dev}`
+let texto2 = packstickers.text2 || `𝐁 𝐎 𝐓\n${botname}\n${fecha}\n${hora}\n╚═════ஜ۩۞۩ஜ═════╝`
 
 try {
 let q = m.quoted ? m.quoted : m
