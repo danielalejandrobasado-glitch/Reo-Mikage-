5f918cd4dca2ec514e98b190e7ff5b67de7313d6
ANDERSONTELLO <jhorditelloarrue@gmail.com>
2026-01-01 23:55:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import fs, { readdirSync, statSync, unlinkSync, existsSync, mkdirSync, readFileS
 import yargs from 'yargs'
 import { spawn, execSync } from 'child_process'
 import lodash from 'lodash'
-import { mikuJadiBot } from './plugins/jadibot-serbot.js'
+//import { mikuJadiBot } from './plugins/jadibot-serbot.js'
 import { checkCodesEndpoint } from './lib/apiChecker.js';
 import chalk from 'chalk'
 import syntaxerror from 'syntax-error'
