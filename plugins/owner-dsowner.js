ec3d3f01960abe7f09a55ec28c94edb2a3ec4b8b
ANDERSONTELLO <jhorditelloarrue@gmail.com>
2026-01-02 22:45:51 -0500

    Update owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -6,9 +6,9 @@ import path from 'path'
 var handler = async (m, { conn, usedPrefix }) => {
 
 if (global.conn.user.jid !== conn.user.jid) {
-return conn.reply(m.chat, `${emoji} Utiliza este comando directamente en el número principal del Bot.`, m)
+return conn.reply(m.chat, `Utiliza este comando directamente en el número principal del Bot.`, m)
 }
-await conn.reply(m.chat, `${emoji2} Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...`, m)
+await conn.reply(m.chat, `➢ Eliminando basura...\n> Excepto el archivo creds.json...`, m)
 m.react(rwait)
 
 let sessionPath = `./${sessions}/`
@@ -30,7 +30,7 @@ if (filesDeleted === 0) {
 await conn.reply(m.chat, `${emoji2} La carpeta esta vacía.`, m)
 } else {
 m.react(done)
-await conn.reply(m.chat, `> ➢ Basura eliminada\n*BASURA ${filesDeleted}*\n\n> Excepto el archivo creds.json.`, m)
+await conn.reply(m.chat, `> ➢ Basura eliminada\n*BASURA ${filesDeleted}\n\n> Excepto el archivo creds.json.`, m)
 conn.reply(m.chat, `> ➢ Si me ves eres gay...`, m)
 
 }
