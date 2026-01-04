5b0b0343be763bb7ed807db72855978fe27a8a5f
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2026-01-04 17:53:33 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -26,7 +26,7 @@ global.botname = '† REO MIKAGE †'
 global.dev = 'Powered by DuarteXv'
 global.author = '© DuarteXV'
 global.textbot = '👑 Mikage, el prodigio absoluto del fútbol japonés.'
-global.banner = 'https://raw.githubusercontent.com/ANDERSONARRUE/Img.2/main/upload_1767565251783.jpg'
+global.banner = 'https://raw.githubusercontent.com/ANDERSONARRUE/Img.2/main/upload_1767566705050.jpg'
 global.prefix = '!'
 
 
