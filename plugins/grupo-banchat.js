6f6364a14735a8a4f3de644d138194794ae7b770
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2026-01-05 17:01:52 -0600

    DEPOOL FIX
    Updated the response message for banning a chat.

--- plugins/grupo-banchat.js
+++ plugins/grupo-banchat.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, isAdmin, isROwner }) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     const chat = global.getChat ? global.getChat(m.chat) : global.db.data.chats[m.chat]
     chat.isBanned = true
-    await conn.reply(m.chat, `💙 Chat Baneado con exito.`, m, global.rcanal)
+    await conn.reply(m.chat, `🚫 Chat Baneado con exito.`, m, global.rcanal)
     await m.react('✅')
 }
 handler.help = ['banearbot']
@@ -11,4 +11,5 @@ handler.command = ['banearbot', 'banchat']
 handler.group = true 
 handler.register = true;
 export default handler
+
 
