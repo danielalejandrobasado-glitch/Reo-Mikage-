8a712b26ea6139bf0b971252cf0123d5320f4455
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2026-01-05 17:02:19 -0600

    DEPOOL FIX

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, isAdmin, isROwner} ) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    await conn.reply(m.chat, '💙 Bot activo en este grupo.', m, global.rcanal)
+    await conn.reply(m.chat, '✅ Bot activo en este grupo.', m, global.rcanal)
     await m.react('✅')
 }
 handler.help = ['desbanearbot']
@@ -9,4 +9,5 @@ handler.tags = ['group']
 handler.command = ['desbanearbot', 'unbanchat']
 handler.group = true 
 export default handler
+
 
