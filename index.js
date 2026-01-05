3bc68e840a9d51b2a1268cb55dccba808c6c9582
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2026-01-04 20:09:54 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 let { say } = cfonts
 console.log(chalk.blueBright('\n● Iniciando...'))
-say('KAISER', {
+say('REO MIKAGE', {
 font: 'simple',
 align: 'left',
 gradient: ['blue', 'white']
