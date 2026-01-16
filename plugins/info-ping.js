fd7b71976224ce95c5d5d21574bc486b2ef7c76a
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2026-01-16 18:41:20 -0500

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -6,10 +6,17 @@ let timestamp = speed()
 let sentMsg = await conn.reply(m.chat, 'Analyzing...', m)
 let latency = speed() - timestamp
 exec(`neofetch --stdout`, (error, stdout, stderr) => {
-let child = stdout.toString("utf-8");
+let child = stdout.toString("utf-8")
 let ssd = child.replace(/Memory:/, "Ram:")
 
-let result = `*¡Pong!*\n> ⴵ Latency ${latency.toFixed(4).split(".")[0]} ms${ssd}`
+let result = `﹒˚ ₊ ︵﹒⊹ ๑ ︵︵ ๑ ⊹﹒︵
+> \`𐔌 . ⋮¡Pong! .ᐟ ֹ ₊ ꒱\`
+
+\`\`\`🍙ྀིྀLatenciaྀིྀི🍙
+﹒˚ ₊ ︵﹒⊹ ๑ ︵︵ ๑ ⊹﹒︵
+
+ⴵ ${latency.toFixed(4).split(".")[0]} ms\`\`\`
+${ssd}`
 conn.sendMessage(m.chat, { text: result, edit: sentMsg.key }, { quoted: m })
 })
 }
