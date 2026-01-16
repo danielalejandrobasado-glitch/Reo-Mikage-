7456e7460f1ca4ee2b3e788eb06994798855c192
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2026-01-16 18:32:29 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,8 @@ global.botNumber = ''
 
 global.owner = [
   ['573135180876', 'DuarteXV'],
-  ['51933000214', 'Ander']
+  ['51933000214', 'Ander'],
+  ['573229506110', 'Naofumi']
 ];
 
 global.mods = []
@@ -16,36 +17,34 @@ global.prems = []
 global.libreria = 'Baileys'
 global.baileys = 'V 6.7.17' 
 global.vs = '1.0.0'
-global.nameqr = '👑 Reo-Mikage-MD 👑'
-global.namebot = 'REO MIKAGE BOT'
+global.nameqr = '🛡️ Naofumi-MD 🛡️'
+global.namebot = 'NAOFUMI BOT'
 global.sessions = 'Sessions'
 global.jadi = 'JadiBots' 
 
-global.packname = '👑 REO MIKAGE BOT 👑'
-global.botname = '† REO MIKAGE †'
+global.packname = '🛡️ NAOFUMI BOT 🛡️'
+global.botname = '† NAOFUMI IWATANI †'
 global.dev = 'Powered by DuarteXv'
 global.author = '© DuarteXV'
-global.textbot = '👑 Mikage, el prodigio absoluto del fútbol japonés.'
-global.banner = 'https://cdn.hostrta.win/fl/h53m.jpg'
+global.textbot = '🛡️ Naofumi Iwatani, el Héroe del Escudo.'
+global.banner = 'https://cdn.hostrta.win/fl/39kd.jpg'
 global.prefix = '!'
 global.welcom1 = 'Bienvenido a la comunidad'
-global.owner = ['573135180876', '51933000214'].map(v => v + '@s.whatsapp.net')
+global.ownerNumbers = ['573135180876', '51933000214', '573229506110'].map(v => v + '@s.whatsapp.net')
 
 global.ch = {
    ch1: '120363420979328566@newsletter',
 }
 
-
 global.isOwner = (sender) => {
   const number = sender.replace('@s.whatsapp.net', '')
   return global.owner.some(owner => owner[0] === number) || 
-         global.creador.some(creator => creator.replace('@s.whatsapp.net', '') === number)
+         global.creador?.some(creator => creator.replace('@s.whatsapp.net', '') === number)
 }
-
 
 global.isCreator = (sender) => {
   const number = sender.replace('@s.whatsapp.net', '')
-  return global.creador.some(creator => creator.replace('@s.whatsapp.net', '') === number)
+  return global.creador?.some(creator => creator.replace('@s.whatsapp.net', '') === number)
 }
 
 let file = fileURLToPath(import.meta.url)
