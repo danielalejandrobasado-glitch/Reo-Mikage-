84c72fb447cb0f0737b0c8317145cda2f1cf9b0d
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2026-01-16 21:27:16 -0500

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -3,17 +3,18 @@ import { spawn, exec, execSync } from 'child_process'
 
 let handler = async (m, { conn }) => {
 let timestamp = speed()
-let sentMsg = await conn.reply(m.chat, 'Analyzing...', m)
+let sentMsg = await conn.reply(m.chat, '🛡️ Evaluando defensas...', m)
 let latency = speed() - timestamp
 exec(`neofetch --stdout`, (error, stdout, stderr) => {
 let child = stdout.toString("utf-8")
 let ssd = child.replace(/Memory:/, "Ram:")
 
-let result = `﹒˚ ₊ ︵﹒⊹ ๑ ︵︵ ๑ ⊹﹒︵
-> \`𐔌 . ⋮¡Pong! .ᐟ ֹ ₊ ꒱\`
+let result = `⚔️ ﹒⊹ ๑ ⊹﹒︵ *ESTADO DEL ESCUDO* ︵﹒⊹ ๑ ⊹﹒ ⚔️
 
-\`\`\`🍙ྀིྀLatenciaྀིྀི🍙
-﹒˚ ₊ ︵﹒⊹ ๑ ︵︵ ๑ ⊹﹒︵
+> \`🛡️ ¡Contraataque detectado! .ᐟ ֹ ₊ ꒱\`
+
+\`\`\`🛡️ TIEMPO DE DEFENSA 🛡️
+﹒⊹ ๑ ⊹﹒︵︵﹒⊹ ๑ ⊹﹒
 
 ⴵ ${latency.toFixed(4).split(".")[0]} ms\`\`\`
 ${ssd}`
@@ -22,6 +23,6 @@ conn.sendMessage(m.chat, { text: result, edit: sentMsg.key }, { quoted: m })
 }
 handler.help = ['ping']
 handler.tags = ['info']
-handler.command = ['ping', 'p']
+handler.command = ['ping', 'evaluardefensas', 'estadoescudo']
 
 export default handler
