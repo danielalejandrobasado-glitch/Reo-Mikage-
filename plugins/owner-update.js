f9b838ce13c9ee7dcef7fec7ac3cdcc2d284041c
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2026-01-18 12:58:29 -0500

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,11 +1,11 @@
 import { exec } from 'child_process';
 
 let handler = async (m, { conn }) => {
-  m.reply(`${emoji2} Actualizando el bot...`);
+  m.reply(`⚛ Actualizando el bot...`);
 
   exec('git pull', (err, stdout, stderr) => {
     if (err) {
-      conn.reply(m.chat, `${msm} Error: No se pudo realizar la actualización.\nRazón: ${err.message}`, m);
+      conn.reply(m.chat, `📜 Error: No se pudo realizar la actualización.\nRazón: ${err.message}`, m);
       return;
     }
 
@@ -14,9 +14,9 @@ let handler = async (m, { conn }) => {
     }
 
     if (stdout.includes('Already up to date.')) {
-      conn.reply(m.chat, `${emoji4} El bot ya está actualizado.`, m);
+      conn.reply(m.chat, `🛡 El bot ya está actualizado.`, m);
     } else {
-      conn.reply(m.chat, `${emoji} Actualización realizada con éxito.\n\n${stdout}`, m);
+      conn.reply(m.chat, `⚛ Actualización realizada con éxito.\n\n${stdout}`, m);
     }
   });
 };
